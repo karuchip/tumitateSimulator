530557627c0ca20eb843d8411fa1543fc482ba24
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-12-16 20:35:02 +0900

    [add]ユーザー入力用コンポーネントを追加

--- app/coastFireAgeOfAchievementPage/page.tsx
+++ app/coastFireAgeOfAchievementPage/page.tsx
@@ -1,6 +1,19 @@
 "use client"
 
-import useCoastFireCalculator from "@/src/function/coastFireAgeOfAchievement/hooks/useCoastFireCalculator";
+import UseCoastFireCalculator from "@/src/function/coastFireAgeOfAchievement/hooks/useCoastFireCalculator";
+import { FormProvider } from "react-hook-form";
+import { InputProps } from "@/src/type/CoastFireAgeOfAchievement/input";
+import { OutputProps } from "@/src/type/CoastFireAgeOfAchievement/output"
+import {useForm} from "react-hook-form"
+import InputItems from "@/app/components/coastFireAgeOfAchievementSection/input/inputItems"
+import { useRef, useState } from "react";
+
+
+type achievedResultProps = {
+  age: number;
+  principal: number;
+  pv: number;
+}
 
 const CoastFireAgeOfAchievement = () => {
 
@@ -13,24 +26,73 @@ const CoastFireAgeOfAchievement = () => {
     retiredAge: 60,
   }
 
-  const {
-    trajectory,
-    coastFireAge,
-    achievedResult
-  } = useCoastFireCalculator(inputs);
+  // シュミレーション結果
+  const [simulationResult, setSimulationResult] = useState<{
+    trajectory: OutputProps[],
+    coastFireAge: number | null,
+    achievedResult: achievedResultProps[] | null,
+  } | null>(null);
+
+  const methods = useForm<InputProps>({ defaultValues: inputs });
+  const { handleSubmit } = methods;
+
+  const onSubmit = (data: InputProps) => {
+      try {
+        const {
+          trajectory,
+          coastFireAge,
+          achievedResult
+        } = UseCoastFireCalculator(data);
+
+        setSimulationResult({
+          trajectory,
+          coastFireAge,
+          achievedResult
+        })
+      }catch(error) {
+        if(error instanceof Error) {
+          alert(error.message);
+        }else {alert("不明なエラーが発生しました")}
+      }
+    }
 
 
   return(
     <>
+
+      {/* <FormProvider {...methods}>
+        <form >
+          <InputItems/>
+        </form>
+      </FormProvider> */}
+      <div className="co-screenPositionInput">
+        <div>
+          {/* 入力 */}
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}  className="SimulatorCoastContainerLeft">
+              <div className="SimulatorCoastInputContainer">
+                <InputItems/>
+              </div>
+
+              <div className="co-simulationButton">
+                <button type="submit">
+                  この内容で<br/>シュミレーションする
+                </button>
+              </div>
+
+            </form>
+          </FormProvider>
+        </div>
+      </div>
       <p>あああ</p>
       <div>
-        {trajectory.map((item, index) => (
+        {simulationResult?.trajectory?.map((item, index) => (
           <pre key={index}>{JSON.stringify(item)}</pre>
         ))}
       </div>
-      <p>{coastFireAge}</p>
+      <p>{simulationResult?.coastFireAge ?? ""}</p>
       <div>
-        {achievedResult?.map((item, index) => (
+        {simulationResult?.achievedResult?.map((item, index) => (
           <pre key={index}>{JSON.stringify(item)}</pre>
         ))}
       </div>
