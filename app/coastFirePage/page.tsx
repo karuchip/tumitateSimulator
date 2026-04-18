00106b82c5808a079b1c5421697de00d0423171c
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2026-04-18 22:54:03 +0900

    [modified] OGP画像が表示されない事象を解消

--- app/coastFirePage/page.tsx
+++ app/coastFirePage/page.tsx
@@ -5,16 +5,19 @@ export const metadata = {
   title: "コーストFIREに必要な積立額はいくら？｜無料シミュレーター",
   description:
     "コーストFIREを達成するために毎月いくら積み立てればよいかを簡単に計算できる無料シミュレーターです。必要元本や利回りをもとに、最適な積立額をシミュレートできます。",
-  openGraph: {
-    title: "コーストFIREに必要な積立額はいくら？",
-    description: "あなたは毎月いくら積み立てれば達成できる？",
-    images: [
+    openGraph: {
+      title: "コーストFIREに必要な積立額はいくら？",
+      description: "あなたは毎月いくら積み立てれば達成できる？",
+      url: "https://fire.hika-design.com/coastFirePage",
+      siteName: "コーストFIREシミュレーター",
+      images: [
       {
         url: "https://fire.hika-design.com/ogp.png",
         width: 1200,
         height: 630,
       },
     ],
+    local: "ja_JP",
     type: "website",
   },
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://fire.hika-design.com/"),
   openGraph: {
     title: "コーストFIREシミュレーター",
     description:
